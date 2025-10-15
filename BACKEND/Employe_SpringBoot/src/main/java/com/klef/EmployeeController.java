f4c18a1062093537b97caf514a4e9c14280e0d1d
Pujitha <pujithashaik93@gmail.com>
2025-10-15 20:43:27 +0530

    Updated Jenkinsfile

--- BACKEND/Employe_SpringBoot/src/main/java/com/klef/EmployeeController.java
+++ BACKEND/Employe_SpringBoot/src/main/java/com/klef/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:2030")
 @RequestMapping("/employeeapi")
 public class EmployeeController {
 
